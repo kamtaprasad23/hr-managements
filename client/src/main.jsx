8ec25fcd073c0aef136ef010eb4fee370ea0b130
Kamtaprasad23 <shahukamtaprasad@gmail.com>
2025-10-23 13:53:34 +0530

    add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,13 @@ import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Toaster position="top-center" reverseOrder={false} />
+    <HashRouter>
     <App />
+    </HashRouter>
   </Provider>
 );
