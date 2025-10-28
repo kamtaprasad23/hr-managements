bdd4b3245dd24362151560a6503ede1d78fc939b
Kamtaprasad23 <shahukamtaprasad@gmail.com>
2025-10-29 00:18:22 +0530

    update

--- server/index.js
+++ server/index.js
@@ -101,13 +101,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// --- Serve React App ---
 const frontendPath = path.join(__dirname, "../client/dist");
+// Serve static files from the React app
 app.use(express.static(frontendPath));
 
+// The "catchall" handler: for any request that doesn't match one above,
+// send back React's index.html file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
-
 // ----------------- Start server -----------------
 const PORT = port || process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
