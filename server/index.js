c2730e13f5cea7aa585514821521a7ff89228292
Kamtaprasad23 <shahukamtaprasad@gmail.com>
2025-10-29 02:43:36 +0530

    changes server

--- server/index.js
+++ server/index.js
@@ -89,5 +89,12 @@ app.use("/api/task", taskRoutes);
 app.use("/api/auth", userRoutes);
 app.use("/api/upload", uploadRoutes);
 
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 const PORT = port || process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
